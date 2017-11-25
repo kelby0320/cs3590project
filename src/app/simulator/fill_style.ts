fef60ee2b0697de9c1d13a28c57d3834062c5c0e
Kelby Madal <kelby0320@gmail.com>
2017-11-25 14:49:15 -0800

    Forgot fill_style.ts

---
+++ src/app/simulator/fill_style.ts
@@ -0,0 +1,6 @@
+export class FillStyle {
+  public static readonly Default = "rgb(0, 0, 0)";
+  public static readonly FrameOK = "rgba(50, 235, 60, 0.4)";
+  public static readonly FrameErr = "rgba(230, 10, 10, 0.4)";
+  public static readonly SlidingWindow = "rgba(175, 50, 190, 0.4)";
+}
